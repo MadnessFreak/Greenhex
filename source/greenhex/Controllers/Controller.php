83da2591ed64c58d1d2cca386319c74061272b6c
MadnessFreak <supervivo@icloud.com>
2016-06-12 11:30:28 +0200

    Optimized controller

--- source/greenhex/Controllers/Controller.php
+++ source/greenhex/Controllers/Controller.php
@@ -2,13 +2,28 @@
 namespace Greenhex\Controllers;
 use Greenhex\Greenhex;
 
-class Controller
+abstract class Controller
 {
+	/**
+	 * @var	Slim
+	 */
 	protected $app;
+	/**
+	 * @var	Database
+	 */
 	protected $db;
+	/**
+	 * @var	Config
+	 */
 	protected $config;
+	/**
+	 * @var	array	Optional template data
+	 */
 	protected $data;
 
+	/**
+	 * Initializes a new instance of the Greenhex\Controllers\Controller
+	 */
 	public function __construct()
 	{
 		$this->app = Greenhex::getApp();
@@ -21,6 +36,11 @@ class Controller
 		$this->data['meta_keywords'] = $this->config->get('greenhex.keywords');
 	}
 
+	/**
+	 * Returns the value at the specified index
+	 *
+	 * @param	string	name
+	 */
 	public function __get($name)
 	{
 		if (isset($this->data[$name]))
@@ -30,11 +50,62 @@ class Controller
 		return null;
 	}
 
+	/**
+	 * Sets a value to the specified index
+	 *
+	 * @param	string	name
+	 * @param	mixed	value
+	 */
 	public function __set($name, $value)
 	{
 		$this->data[$name] = $value;
 	}
 
+	/**
+	 * Sets the response content type
+	 *
+	 * @param	string	contentType
+	 */
+	public function contentType($contentType)
+	{
+		$this->response()->header('Content-Type', $contentType);
+	}
+
+	/**
+	 * Redirects to the specified path
+	 *
+	 * @param	string	$path
+	 */
+	public function redirect($path)
+	{
+		$this->app->redirect($path);
+	}
+
+	/**
+	 * Slim request object
+	 *
+	 * @return	\Slim\Http\Request
+	 */
+	public function request()
+	{
+		return $this->app->request();
+	}
+
+	/**
+	 * Slim response object
+	 *
+	 * @return	\Slim\Http\Response
+	 */
+	public function response()
+	{
+		return $this->app->response();
+	}
+
+	/**
+	 * Renders the specified template
+	 *
+	 * @param	string	$template
+	 */
 	public function display($template)
 	{
 		$this->app->render($template, $this->data);
